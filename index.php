ac7cbd39e602b65c9182ba2f4d2b53e66cdb0b0d
erik684 <eriktails@yahoo.com.br>
2017-12-21 23:07:00 -0200

    created home and footer in Views folder

--- index.php
+++ index.php
@@ -26,14 +26,7 @@
 	<header ng-include="'views/header.php'"></header>
 	
 	<main ui-view></main>
-
-	<hr class="divider">
-	
-	<!-- FOOTER -->
-	<footer class="footer stick-footer">	
-		<span class="text-muted"><i>Erik Aleixo:</i> </span>	  
-		<a class="btn btn-link" href="https:\\www.github.com/erik684/php-login-consulta">github.com/erik684/php-login-consulta</a>
-	</footer>
+	<footer ng-include="'views/footer.php'"></footer>
 
 <!-- FIM CONTAINER PRINCIPAL -->
 </div>
---
+++ views/footer.php
@@ -0,0 +1,6 @@
+	<hr class="divider">
+	<!-- FOOTER -->
+	<footer class="footer stick-footer">	
+		<span class="text-muted"><i>Erik Aleixo:</i> </span>	  
+		<a class="btn btn-link" href="https:\\www.github.com/erik684/php-login-consulta">github.com/erik684/php-login-consulta</a>
+	</footer>
